e5d8563778776d979c6e01cdfeb92fd138787533
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-12 18:35:27 +0900

    feat: MVC로 파일 나누고 시작 / Model 생성자 추가

---
+++ contactList/Models/Member.swift
@@ -0,0 +1,48 @@
+//
+//  Member.swift
+//  contactList
+//
+//  Created by 이명진 on 2023/07/12.
+//
+
+import UIKit
+
+struct Member {
+    
+    lazy var memberImage: UIImage? = {
+        
+        // 이름이 없으면, 기본이미지 추가
+        guard let name = name else {
+            return UIImage(systemName: "person") // nil
+        }
+        
+        // 해당 이름으로된 이미지가 없으면 기본 이미지 세팅 nil x
+        return UIImage(named: "\(name).png") ?? UIImage(systemName: "person")
+    }()
+    
+    // 멤버의 순서를 위한 타입 저장 속성
+    static var memberNumbers: Int = 0
+    
+    let memberId: Int
+    var name:String?
+    var age: Int?
+    var phone: String?
+    var address: String?
+    
+    init(name: String?, age: Int?, phone: String?, address: String?) {
+        
+        // 0 일때는 0, 0이 아니면 늘어난 숫자를 가지고 세팅함
+        self.memberId = Member.memberNumbers == 0 ? 0 : Member.memberNumbers
+        
+        
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
+        
+        // 멤버 생성시, memberNumbers 저장 속성 += 1
+        Member.memberNumbers += 1
+    }
+    
+    
+}
