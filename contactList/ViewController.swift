e148d45728200a054ca50ed4d4846fe38eb5493e
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-12 17:52:55 +0900

    Initial Commit

---
+++ contactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactList
+//
+//  Created by 이명진 on 2023/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
