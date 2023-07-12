948ccb1d0f2e2633dc7a02b16e19bf035a1d7b51
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-12 21:36:02 +0900

    feat: 전 화면 접근 ViewController.count() - 2

--- contactList/Controllers/DetailViewController.swift
+++ contactList/Controllers/DetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class DetailViewController: UIViewController {
-
+    
     private let detailView = DetailView()
     
     var member: Member?
@@ -32,7 +32,70 @@ final class DetailViewController: UIViewController {
         detailView.saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
     }
     
+    
+    //MARK: - SAVE버튼 또는 UPDATE버튼이 눌렸을때의 동작
+    
     @objc func saveButtonTapped(){
         print("버튼 눌림")
+        
+        // [1] 멤버가 없다면 (새로운 멤버를 추가하는 화면)
+        if member == nil {
+            // 입력이 안되어 있다면.. (일반적으로) 빈문자열로 저장
+            let name = detailView.nameTextField.text ?? ""
+            let age = Int(detailView.ageTextField.text ?? "")
+            let phoneNumber = detailView.phoneNumberTextField.text ?? ""
+            let address = detailView.addressTextField.text ?? ""
+            
+            // 새로운 멤버 (구조체) 생성
+            var newMember =
+            Member(name: name, age: age, phone: phoneNumber, address: address)
+            newMember.memberImage = detailView.mainImageView.image
+            
+            // 1) 델리게이트 방식이 아닌 구현⭐️
+            let index = navigationController!.viewControllers.count - 2
+            // 전 화면에 접근하기 위함
+            let vc = navigationController?.viewControllers[index] as! ViewController
+            // 전 화면의 모델에 접근해서 멤버를 추가
+            vc.memberListManager.makeNewMembers(newMember)
+            
+            
+            // 2) 델리게이트 방식으로 구현⭐️
+            //delegate?.addNewMember(newMember)
+            
+            
+            // [2] 멤버가 있다면 (멤버의 내용을 업데이트 하기 위한 설정)
+        } else {
+            // 이미지뷰에 있는 것을 그대로 다시 멤버에 저장
+            member!.memberImage = detailView.mainImageView.image
+            
+            let memberId = Int(detailView.memberIdTextField.text!) ?? 0
+            member!.name = detailView.nameTextField.text ?? ""
+            member!.age = Int(detailView.ageTextField.text ?? "") ?? 0
+            member!.phone = detailView.phoneNumberTextField.text ?? ""
+            member!.address = detailView.addressTextField.text ?? ""
+            
+            // 뷰에도 바뀐 멤버를 전달 (뷰컨트롤러 ==> 뷰)
+            detailView.member = member
+            
+            // 1) 델리게이트 방식이 아닌 구현⭐️
+            let index = navigationController!.viewControllers.count - 2 // (count-1) - 1 이 전 화면 접근
+            // 전 화면에 접근하기 위함
+            let vc = navigationController?.viewControllers[index] as! ViewController // "0" 번째 화면 count-1 이 가장 마지막
+            // 전 화면의 모델에 접근해서 멤버를 업데이트
+            vc.memberListManager.updateMemberInfo(index: memberId, member!)
+            
+            
+            // 델리게이트 방식으로 구현⭐️
+            //delegate?.update(index: memberId, member!)
+        }
+        
+        // (일처리를 다한 후에) 전화면으로 돌아가기
+        self.navigationController?.popViewController(animated: true)
     }
+    
+    deinit {
+        print("디테일 뷰컨트롤러 해제")
+    }
+    
 }
+
