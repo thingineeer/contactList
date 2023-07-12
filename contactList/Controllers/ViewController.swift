a469af73781500d79227a2c0b0f5ee5805dd260b
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-12 18:54:44 +0900

    feat: Navigator 추가

--- contactList/Controllers/ViewController.swift
+++ contactList/Controllers/ViewController.swift
@@ -12,6 +12,8 @@ final class ViewController: UIViewController {
     // 테이블뷰 생성
     private let tableView = UITableView()
     
+    var memberListManager = MemberListManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +21,22 @@ final class ViewController: UIViewController {
         
         setupTableViewConstraints()
         
+    }
+    
+    func setupNaviBar() {
+        title = "회원 목록"
+        
+        // 네비게이션바 설정관련
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()  // 불투명으로
+        appearance.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .systemBlue
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        // 네비게이션바 오른쪽 상단 버튼 설정
+        self.navigationItem.rightBarButtonItem = self.plusButton
     }
     
     func setupTableViewConstraints() {
@@ -40,7 +58,7 @@ final class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return memberListManager.getMemberList().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- contactList/Models/memberListManager.swift
+++ contactList/Models/memberListManager.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class memberListManager {
+final class MemberListManager {
     
     private var membersList : [Member] = []
     
